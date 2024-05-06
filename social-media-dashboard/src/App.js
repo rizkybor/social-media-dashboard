a7a1396c5e2141f0dbf6df7df1b2bea347a56134
Rizky Ajie Kurniawan - Mac <rizkyak994@gmail.com>
2024-05-07 02:07:59 +0700

    update substance

--- social-media-dashboard/src/App.js
+++ social-media-dashboard/src/App.js
@@ -17,21 +17,6 @@ function App() {
           <CardList2></CardList2>
         </section>
       </Grid>
-      <Typography
-        textAlign={"center"}
-        fontSize={11}
-        color={"var(--text-color)"}
-      >
-        Challenge by
-        <a
-          color={"var(--text-color2)"}
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="_blank"
-        >
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </Typography>
     </div>
   );
 }
